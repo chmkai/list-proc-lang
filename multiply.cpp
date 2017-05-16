007dbaa09438cd18f7fc94e8861930041ca0fea3
Kolkonut <daanishbaig@hotmail.com>
2017-05-16 20:01:10 +0300

    Update multiply.cpp

--- multiply.cpp
+++ multiply.cpp
@@ -3,7 +3,7 @@ atom multiply(func f)
   int prod=1;
   for (int i=1,i<9;i++)
     if((f.arg(i)).rtype() == 'n')
-      prod*=f.arg[i];
+      prod*=f.arg(i);
   atom x;
   x.setval(prod,'n');
   return x;
